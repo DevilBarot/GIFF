5dc81b981bd3a59819222b547b4b3a8b01154897
dhaval <dhavalb2796>
2018-09-21 15:41:45 +0530

    testchanges2

--- app/src/main/java/com/example/ap1/giff/MainActivity.java
+++ app/src/main/java/com/example/ap1/giff/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
                 showGifView.drawGif();
                 //Lolamlol
 
-                //jsghdcjy gaikuchkajsncdw
+                //jsghdcjy gaikuchkajsncdwkhjabgckabghs
             }
         });
 
